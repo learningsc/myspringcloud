d57723790ebe61b466d7a4de1d0105cdb4708c78
fyk <747765358@qq.com>
2017-09-13 14:38:59 +0800

    服务提供者

---
+++ service-hi/src/main/java/com/ServiceHiApplication.java
@@ -0,0 +1,26 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient // 表示这个类为服务提供者客户端
+@RestController
+public class ServiceHiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceHiApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+    @RequestMapping("/hi")
+    public String home(@RequestParam String name) {
+        return "hi "+name+",i am from port:" +port;
+    }
+}
